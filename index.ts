fcb9bb8978adc7fdfc8f37b8b5896c1f0c174a65
Faissal EL OSMAN <faissal.elosman@gmail.com>
2025-10-20 19:50:01 +0000

    Add index.ts for exporting all DTOs

---
+++ index.ts
@@ -0,0 +1,8 @@
+// User DTOs
+export * from './dto/user/create-user.dto';
+export * from './dto/user/login.dto';
+
+// Tenant DTOs
+export * from './dto/tenant/create-tenant.dto';
+export * from './dto/tenant/update-tenant.dto';
+export * from './dto/tenant/update-status.dto';
